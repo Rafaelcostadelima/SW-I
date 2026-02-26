e4bc23a1bb7802231bebdc7b1bcfe1a9742b4573
Rafael Lima <rafita.limagames2@gmail.com>
2026-02-26 08:51:41 -0300

    Add files via upload

---
+++ aula03/exemplo03.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividades</title>
+</head>
+<body>
+    <h1>Exercício 7</h1>
+    <?php
+        // $a = "Casal 20";
+         // $b = 10;
+        // $c = $a + $b;
+        // echo $c;
+        // Resposta D
+    ?>
+    <p>Resposta D</p>
+    <br>
+    <h1>Exercício 8</h1>
+    <?php
+        $pre = "hiper";
+        // echo "$pre.texto";
+        // echo $pre"texto";
+        // echo "$pretexto";
+        // echo "${pre}texto";
+        // Resposta D
+    ?>
+    <p>Resposta D</p>
+    <br>
+    <h1>Exercício 9</h1>
+    <p></p>
+</body>
+</html>
---
+++ aula03/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AULA 03</title>
+</head>
+
+<body>
+    <h1>AULA 03 - SW-I</h1>
+    <?php
+        $nome = "Rafael Original";
+        echo "<p style='color: red;'>Seja bem vindo, $nome!</p>";
+    ?>
+    <p>Nome: <?php echo $nome;?></p>
+</body>
+
+</html>
